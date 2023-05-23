2fb5072f7cdd7f7421e15f4d316de6d1b8f0c4df
sohail <msohailkhan.se@gmail.com>
2023-05-23 15:15:05 +0500

    PHP Generator

---
+++ app/Generator.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "<h3>Generator</h3>";
+
+function fib($n)
+{
+    $cur = 1;
+    $prev = 0;
+    for ($i = 0; $i < $n; $i++) {
+        yield $cur;
+
+        $temp = $cur;
+        $cur = $prev + $cur;
+        $prev = $temp;
+    }
+}
+
+$fibs = fib(9);
+foreach ($fibs as $fib) {
+    echo " " . $fib;
+}
